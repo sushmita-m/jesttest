e7844b9c67000aee7c69c61d1dc1d7603663b6d3
sushmita-m <sushmita-m@github.com>
2022-03-24 10:39:20 +0530

    more tests

--- example.ts
+++ example.ts
@@ -1,3 +1,7 @@
 export function sum(a,b){
     return a+b;
 }
+
+export function sumNeg(a,b){
+    return -1*(a+b);
+}
--- test/example.test.ts
+++ test/example.test.ts
@@ -1,8 +1,13 @@
-import { sum } from '../example'
+import { sum, sumNeg } from '../example'
 
 describe('additon', () => {
     test('test two postive number',  () => {
         const sumPos = sum(1,1);
         expect(sumPos).toEqual(2);
+    });
+
+    test ('neg sum',  () => {
+        const sumPos = sumNeg(1,1);
+        expect(sumPos).toEqual(-2);
     });
 });
